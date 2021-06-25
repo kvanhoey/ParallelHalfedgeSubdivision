94c533e2c62ff68d9b61763d8d42246cba0062ce
Kenneth Vanhoey <kennethv@unity3d.com>
2021-06-25 17:55:49 +0200

    New results

--- scripts_for_paper_figures/bench_loop_cpu.sh
+++ scripts_for_paper_figures/bench_loop_cpu.sh
@@ -3,6 +3,6 @@ for CPU in 1 2 4 8 16 24; do
 	export OMP_NUM_THREADS=$CPU
 	echo "$CPU CPUs"
 	for D in {1..6}; do
-		./bench_loop data/tri/bigguy.obj $D;
+		./bench_loop bigguy.obj $D;
 	done
 done
