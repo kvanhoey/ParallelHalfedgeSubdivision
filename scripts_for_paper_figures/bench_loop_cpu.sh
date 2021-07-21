e05e82b67d2678b1ed11ca60eb0e7ad82625e157
Kenneth Vanhoey <kennethv@unity3d.com>
2021-07-21 14:17:13 +0200

    script

--- scripts_for_paper_figures/bench_loop_cpu.sh
+++ scripts_for_paper_figures/bench_loop_cpu.sh
@@ -4,6 +4,8 @@ for mesh in ../data_benching/*.obj; do
 		export OMP_NUM_THREADS=$CPU
 		echo "$CPU CPUs"
 		for D in {1..7}; do
-			./bench_loop $mesh $D 50;
+			../build/bench_loop $mesh $D 50;
 		done
+	done
 done
+
