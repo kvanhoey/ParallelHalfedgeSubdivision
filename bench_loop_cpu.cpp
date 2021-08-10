0a79a52e46512e79d29b10bf3a4a6fb152ffa814
Kenneth Vanhoey <kenneth@research.kvanhoey.eu>
2021-08-10 13:58:23 +0200

    .

--- bench_loop_cpu.cpp
+++ bench_loop_cpu.cpp
@@ -63,9 +63,41 @@ int main(int argc, char* argv[])
 
 			S.export_to_obj(ss.str()) ;
 		}
+
+
+
+        // write into files
+        const std::string f_name_tmp = f_name.substr(f_name.find_last_of("\\/") + 1, 999) ;
+        const std::string f_name_clean = f_name_tmp.substr(0,f_name_tmp.find_last_of(".")) ;
+        const char* num_threads_str = std::getenv("OMP_NUM_THREADS") ;
+        int num_threads = 0 ;
+        if (num_threads_str != NULL)
+             num_threads = atoi(num_threads_str) ;
+    //	std::cout << "Using " << num_threads << " threads" << std::endl ;
+
+        std::stringstream fname_he, fname_cr, fname_vx ;
+        fname_he << f_name_clean << "_halfedge_" << d << "_" << num_threads << ".txt" ;
+        fname_cr << f_name_clean << "_crease_" << d << "_" << num_threads << ".txt" ;
+        fname_vx << f_name_clean << "_points_" << d << "_" << num_threads << ".txt" ;
+
+        std::ofstream f_he ;
+        f_he.open(fname_he.str()) ;
+        f_he << std::fixed << refine_he_time.median << "\t/\t" << refine_he_time.mean << "\t/\t" << refine_he_time.min << "\t/\t" << refine_he_time.max << std::endl ;
+        f_he.close() ;
+
+        std::ofstream f_cr ;
+        f_cr.open(fname_cr.str()) ;
+        f_cr << std::fixed << refine_cr_time.median << "\t/\t" << refine_cr_time.mean << "\t/\t" << refine_cr_time.min << "\t/\t" << refine_cr_time.max << std::endl ;
+        f_cr.close() ;
+
+        std::ofstream f_vx ;
+        f_vx.open(fname_vx.str()) ;
+        f_vx << std::fixed << refine_vx_time.median << "\t/\t" << refine_vx_time.mean << "\t/\t" << refine_vx_time.min << "\t/\t" << refine_vx_time.max << std::endl ;
+        f_vx.close() ;
+
     }
 
-//	std::cout << "Halfedges:\t" << refine_he_time << "ms" << std::endl ;
+/*//	std::cout << "Halfedges:\t" << refine_he_time << "ms" << std::endl ;
 //	std::cout << "Creases:\t" << refine_cr_time << "ms" << std::endl ;
 //	std::cout << "Vertices:\t" << refine_vx_time << "ms" << std::endl ;
 	std::cout << std::fixed << refine_he_time.median << "\t" << refine_cr_time.median << "\t" << refine_vx_time.median << std::endl ;
@@ -97,7 +129,7 @@ int main(int argc, char* argv[])
 	std::ofstream f_vx ;
 	f_vx.open(fname_vx.str()) ;
 	f_vx << std::fixed << refine_vx_time.median << "\t/\t" << refine_vx_time.mean << "\t/\t" << refine_vx_time.min << "\t/\t" << refine_vx_time.max << std::endl ;
-	f_vx.close() ;
+    f_vx.close() ;*/
 
 
 	// print the number of threads
