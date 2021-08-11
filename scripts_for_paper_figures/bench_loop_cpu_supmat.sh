60811d2f7cd959e3eb89d70ab6d8321cfff6ec85
Kenneth Vanhoey <kenneth@research.kvanhoey.eu>
2021-08-11 14:11:21 +0200

    update scripts

--- scripts_for_paper_figures/bench_loop_cpu_supmat.sh
+++ scripts_for_paper_figures/bench_loop_cpu_supmat.sh
@@ -3,7 +3,7 @@ for mesh in ../meshes/data_benching/*.obj; do
 	for CPU in 32 16 8 4 2 1; do
 		export OMP_NUM_THREADS=$CPU
 		echo "$CPU CPUs"
-		for D in 6; do
+		for D in {1..7}; do
 			../build/bench_loop_cpu $mesh $D 0 50;
 		done
 	done
