6dff8999fa42b985850098a17e10f3755ff3b680
Kenneth Vanhoey <kenneth@research.kvanhoey.eu>
2021-09-01 15:39:32 +0200

    code cleanup

--- lib/mesh_subdiv_catmull-clark_cpu.cpp
+++ lib/mesh_subdiv_catmull-clark_cpu.cpp
@@ -194,169 +194,26 @@ Mesh_Subdiv_CatmullClark_CPU::refine_vertices_vertexpoints(uint d)
 		vx_sharpness /= float(vx_edge_valence) ;
 
 		bool vx_is_border = h_id_it < 0 ;
-
-		// corner vertex
 		const int vx_halfedge_valence = vx_edge_valence + (vx_is_border ? -1 : 0) ;
+
 		const vec3 increm_corner = old_vx_pt / float(vx_halfedge_valence) ; // corner vertex rule: C.3
-
-		// smooth rule: C.2
 		const vec3 increm_smooth = (4.0f * new_edge_pt - new_face_pt + (float(vx_edge_valence) - 3.0f) * old_vx_pt) / float (vx_edge_valence*vx_edge_valence) ; // Smooth rule: C.2
-
-		// crease rule
 		vec3 increm_creased = c_sharpness_sgn * 0.25f * (new_edge_pt + old_vx_pt) ; // Creased vertex rule: C.5
 		if (vx_is_border)
 		{
 			increm_creased = increm_creased + 0.25f * sgn(prev_sharpness) * (new_prev_edge_pt + old_vx_pt) ;
 		}
 
-		// Blend: C.4
-		float lerp_alpha = vx_sharpness ;
-
 		// apply the right incrementation
 		vec3 increm ;
 		if ((vx_edge_valence == 2) || (vx_n_creases > 2))
-		{
 			increm = increm_corner ;
-		}
 		else if (vx_n_creases < 2)
-		{
 			increm = increm_smooth ;
-		}
 		else // vx_n_creases = 2
-		{
-			increm = lerp(increm_corner, increm_creased, lerp_alpha) ;
-		}
+			increm = lerp(increm_corner, increm_creased, vx_sharpness) ;
 
 		apply_atomic_vec3_increment(new_vx_pt, increm) ;
 	}
-
-//		if (n < 0) // Boundary rule: C.1
-//		{
-//			const float vertex_he_valence = float(vertex_halfedge_valence(H_old, h_id)) ;
-//			const vec3 increm_border = old_vx_point / vertex_he_valence ;
-//			apply_atomic_vec3_increment(new_vx_point, increm_border) ;
-//		}
-//		else
-//		{
-//			const int n_creases = vertex_crease_valence_or_border(H_old, C_old, h_id) ;
-//			if ((n == 2) || n_creases > 2) // Corner vertex rule: C.3
-//			{
-//				const float vx_halfedge_valence = 1.0f / float(vertex_halfedge_valence(H_old, h_id)) ;
-//				const vec3 increm_corner = vx_halfedge_valence * old_vx_point ; // Corner vertex rule: C.3
-//				apply_atomic_vec3_increment(new_vx_point, increm_corner) ;
-//			}
-//			else
-//			{
-//				const float vx_sharpness = n_creases < 2 ? 0.0f : vertex_sharpness_or_border(H_old, C_old, h_id) ; // n_creases < 0 ==> dart vertex ==> smooth
-//				if (vx_sharpness < 1e-6) // Smooth rule: C.2
-//				{
-//					//const int i = Vd + Face(h) ;
-//					const int new_edge_pt_id = Vd + Fd + Edge(H_old, h_id) ;
-//					const int new_face_pt_id = Vd + Face(h_id) ;
-
-//					const float n_ = float(n) ;
-//					const float _n2 = 1.0f / float(n*n) ;
-
-//					const vec3& new_face_pt = V_new[new_face_pt_id] ;
-//					const vec3& new_edge_pt = V_new[new_edge_pt_id] ;
-
-//					const vec3 increm_smooth = (4.0f * new_edge_pt - new_face_pt + (n_ - 3.0f)*old_vx_point) * _n2 ; // Smooth rule: C.2
-//					apply_atomic_vec3_increment(new_vx_point, increm_smooth) ;
-//				}
-//				else // creased case
-//				{
-//					const int c_id = Edge(H_old, h_id) ;
-//					float edge_sharpness = Sharpness(C_old, c_id) ;
-//					if (edge_sharpness > 1e-6) // current edge is crease and contributes
-//					{
-//						const int new_edge_pt_id = Vd + Fd + Edge(H_old, h_id) ;
-//						const vec3& new_edge_pt = V_new[new_edge_pt_id] ;
-
-//						if (vx_sharpness > 1.0)
-//						{
-//							const vec3 increm_creased = 0.25f * (new_edge_pt + old_vx_point) ; // Creased vertex rule: C.5
-//							apply_atomic_vec3_increment(new_vx_point, increm_creased) ;
-//						}
-//						else // Blended vertex rule: C.4
-//						{
-//							const vec3 increm_creased = 0.25f * (new_edge_pt + old_vx_point) ; // Creased vertex rule: C.5
-//							const vec3 increm_corner = 0.5f * old_vx_point ;
-//							const vec3 increm = lerp(increm_corner,increm_creased,vx_sharpness) ;
-//							apply_atomic_vec3_increment(new_vx_point, increm) ;
-//						}
-//					}
-//				}
-//			}
-//		}
-//	}
 	_BARRIER
-
-//	_PARALLEL_FOR
-//	for (int h_id = 0; h_id < Hd ; ++h_id)
-//	{
-//		const int n = vertex_edge_valence_or_border(H_old, h_id) ;
-//		const int v_id = Vert(H_old, h_id) ;
-//		vec3& new_vx_point = V_new[v_id] ;
-//		const vec3& old_vx_point = V_old[v_id] ;
-
-//		if (n < 0) // Boundary rule: C.1
-//		{
-//			const float vertex_he_valence = float(vertex_halfedge_valence(H_old, h_id)) ;
-//			const vec3 increm_border = old_vx_point / vertex_he_valence ;
-//			apply_atomic_vec3_increment(new_vx_point, increm_border) ;
-//		}
-//		else
-//		{
-//			const int n_creases = vertex_crease_valence_or_border(H_old, C_old, h_id) ;
-//			if ((n == 2) || n_creases > 2) // Corner vertex rule: C.3
-//			{
-//				const float vx_halfedge_valence = 1.0f / float(vertex_halfedge_valence(H_old, h_id)) ;
-//				const vec3 increm_corner = vx_halfedge_valence * old_vx_point ; // Corner vertex rule: C.3
-//				apply_atomic_vec3_increment(new_vx_point, increm_corner) ;
-//			}
-//			else
-//			{
-//				const float vx_sharpness = n_creases < 2 ? 0.0f : vertex_sharpness_or_border(H_old, C_old, h_id) ; // n_creases < 0 ==> dart vertex ==> smooth
-//				if (vx_sharpness < 1e-6) // Smooth rule: C.2
-//				{
-//					//const int i = Vd + Face(h) ;
-//					const int new_edge_pt_id = Vd + Fd + Edge(H_old, h_id) ;
-//					const int new_face_pt_id = Vd + Face(h_id) ;
-
-//					const float n_ = float(n) ;
-//					const float _n2 = 1.0f / float(n*n) ;
-
-//					const vec3& new_face_pt = V_new[new_face_pt_id] ;
-//					const vec3& new_edge_pt = V_new[new_edge_pt_id] ;
-
-//					const vec3 increm_smooth = (4.0f * new_edge_pt - new_face_pt + (n_ - 3.0f)*old_vx_point) * _n2 ; // Smooth rule: C.2
-//					apply_atomic_vec3_increment(new_vx_point, increm_smooth) ;
-//				}
-//				else // creased case
-//				{
-//					const int c_id = Edge(H_old, h_id) ;
-//					float edge_sharpness = Sharpness(C_old, c_id) ;
-//					if (edge_sharpness > 1e-6) // current edge is crease and contributes
-//					{
-//						const int new_edge_pt_id = Vd + Fd + Edge(H_old, h_id) ;
-//						const vec3& new_edge_pt = V_new[new_edge_pt_id] ;
-
-//						if (vx_sharpness > 1.0)
-//						{
-//							const vec3 increm_creased = 0.25f * (new_edge_pt + old_vx_point) ; // Creased vertex rule: C.5
-//							apply_atomic_vec3_increment(new_vx_point, increm_creased) ;
-//						}
-//						else // Blended vertex rule: C.4
-//						{
-//							const vec3 increm_creased = 0.25f * (new_edge_pt + old_vx_point) ; // Creased vertex rule: C.5
-//							const vec3 increm_corner = 0.5f * old_vx_point ;
-//							const vec3 increm = lerp(increm_corner,increm_creased,vx_sharpness) ;
-//							apply_atomic_vec3_increment(new_vx_point, increm) ;
-//						}
-//					}
-//				}
-//			}
-//		}
-//	}
-//	_BARRIER
 }
